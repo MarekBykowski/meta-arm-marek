7f79a356e9ce0782f083dbe141d4f4f1c7f60b97
Marek Bykowski <marek.bykowski@gmail.com>
2026-07-04 14:30:39 +0000

    scripts/run.sh: Enable loggin from the console to /tmp
    Signed-off-by: Marek Bykowski <marek.bykowski@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,8 +12,11 @@ pathadd() {
 }
 pathadd ~/Downloads/FVP_Base_RevC-2xAEMvA_11.29_27_Linux64/Base_RevC_AEMvA_pkg/models/Linux64_GCC-9.3
 
+# tee UART0 (Linux console) to a file for post-mortem inspection
+UART0_LOG=/tmp/fvp-uart0.log
+
 if [[ $1 == armds ]]; then
-    "$RUNFVP" --verbose "$FVPCONF" -- --iris-server --iris-port 7101 --iris-allow-remote
+    "$RUNFVP" --verbose "$FVPCONF" -- --parameter "bp.pl011_uart0.out_file=$UART0_LOG" --iris-server --iris-port 7101 --iris-allow-remote
 else
-    "$RUNFVP" --verbose "$FVPCONF"
+    "$RUNFVP" --verbose "$FVPCONF" -- --parameter "bp.pl011_uart0.out_file=$UART0_LOG"
 fi
